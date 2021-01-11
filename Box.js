104fcf18cb1adc2d8591c70c28d303b2edd3f246
Soustin <70994322+Soustin@users.noreply.github.com>
2021-01-11 20:15:56 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,15 +1,16 @@
-class Paper {
-    constructor(x, y, r) {
+class Box {
+    constructor(x, y, width, height) {
       var options = {
-          //isStatic:false,
+          isStatic:true,
           'restitution':0.3,
           'friction':0.5,
           'density':1.2
       }
       this.x=x;
       this.y=y;
-      this.r=r;
-      this.body = Bodies.circle(this.x, this.y, this.r/2, options);
+      this.width=width;
+      this.height=height;
+      this.body = Bodies.rectangle(this.x, this.y, this.width, this.height, options);
 
       World.add(world, this.body);
     }
@@ -18,9 +19,9 @@ class Paper {
       
       push();
       translate(pos.x, pos.y);
-      ellipseMode(CENTER);
-      fill(255, 0, 255);
-      ellipse(pos.x, pos.y, this.r/2);
+      rectMode(CENTER);
+      fill("white");
+      rect(this.x, this.y, this.width, this.height);
       pop();
     }
   };
--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
 function preload()
 {
@@ -16,7 +17,11 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-    paper = new Paper(200, 500);
+    paper = new Paper(70, 280, 100);
+    box = new Box(245, 265, 10, 70);
+    box2 = new Box(300, 265, 10, 70);
+    box3 = new Box(272.5, 280, 99, 10);
+    ground = new Ground(200, 285, width, 10);
 
 	Engine.run(engine);
   
@@ -24,15 +29,25 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
   background(0);
   Engine.update(engine);
-  
+  rectMode(CENTER);
+
   paper.display();
+  box.display();
+  box2.display();
+  box3.display();
+  ground.display();
   
   drawSprites();
+  keyPressed();
  
 }
 
+function keyPressed() {
+  if(keyCode = UP_ARROW){
 
+    Matter.Body.applyForce(paper.body, paper.body.position, {x: 83, y: 83});
 
+  }
+}
